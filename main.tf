80e4391a4a308eb997a903d742686d8c00ab02ed
Ian Allison <iana@pims.math.ca>
2020-09-15 17:29:46 -0700

    Broaden the filter on AMI
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-2.0.20200904.0-x86_64-gp2"]
+    values = ["amzn2-ami-hvm-2.*-x86_64-gp2"]
   }
 
   filter {
